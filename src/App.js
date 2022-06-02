47baf05187126839e40d43bd17bddb933737de50
tezcan <tezcan@tezcans-MBP.home>
2022-06-02 23:03:21 +0300

    This may not be the final commit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
   return (
     <>
       <div className='container'>
-        <h1 style={{ marginLeft: '25px', marginTop: '25px' }}>Greeting 001</h1>
+        <h1 style={{ marginLeft: '25px', marginTop: '25px' }}>Greeting 101</h1>
         <h5 style={{ marginLeft: '25px' }}>
           Move cursor and click to say hello in a different language :)
         </h5>
